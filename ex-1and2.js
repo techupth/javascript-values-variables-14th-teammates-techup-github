d3c8a433f32bca97a1c2cd446f6f0f745f9ffc73
Parot Ketpong <parot@mtrgroup.local>
2023-09-13 14:45:17 +0700

    Assignment : Values Variables: Ex 1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter"
+let surname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurantName ="hef's Table by Chef James"
+const Total = 15000
+const countRestaurantOpen = 1500
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+Total = 5000
+countRestaurantOpen = 2500
